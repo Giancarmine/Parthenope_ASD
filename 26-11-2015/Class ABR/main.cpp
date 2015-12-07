b7c6a63d6c53974cf2289fa46fada2c5bb717b67
Giancarmine <giancarmine.cuofano@gmail.com>
2015-12-07 19:46:52 +0100

    Menù Finished

--- 26-11-2015/Class ABR/main.cpp
+++ 26-11-2015/Class ABR/main.cpp
@@ -25,35 +25,108 @@ int main() {
     do{
         //Pulisco lo schermo -Win/Linux-
         system(CLEAR);
-        cout << "/======================Gestione Albero Binario Di Ricerca===============" << endl;
+        cout << "======================Gestione Albero Binario Di Ricerca======================" << endl;
         cout << endl << endl;
         cout << " [1] - Inserisci Elemento nell`Albero" << endl;
         cout << " [2] - Stampa l`ABR" << endl;
         cout << " [3] - Elimina Elemento dall`Albero" << endl;
-        cout << " [4] - Ricerca" << endl;
-        cout << " [3] - Elimina Elemento dall`Albero" << endl;
-        cout << " [4] - Ricerca" << endl;
+        cout << " [4] - Ricerche" << endl;
+        cout << " [5] - Rotazioni" << endl;
         cout << endl << endl;
         cout << " [0] - Esci" << endl << endl;
         cout << "Seleziona : ";
         cin >> Tasto;
-        switch( Tasto )
-        {
-            case 1 :
+        switch( Tasto ){
+            case 1 :    //Inserimento
                 system(CLEAR);
                 break;
-            case 2 :
+            case 2 :    //Stampa l`ABR
+                do{
+                    system(CLEAR);
+                    cout << "======================Stampa l`ABR======================" << endl;
+                    cout << endl << endl;
+                    cout << " [1] - Stampa IN-Order" << endl;
+                    cout << " [2] - Stampa POST-Order" << endl;
+                    cout << " [3] - Stampa PRE-Order" << endl;
+                    cout << endl << endl;
+                    cout << " [0] - Esci" << endl << endl;
+                    cout << "Seleziona : ";
+                    cin >> Tasto;
+                    switch( Tasto ){
+                        case 1 :
+                            system(CLEAR);
+                            break;
+                        case 2 :
+                            system(CLEAR);
+                            break;
+                        case 3 :
+                            system(CLEAR);
+                            break;
+                    }
+                }
+                while ( Tasto != 0 );
+                cin.clear();
+                Tasto = -1;
+                break;
+            case 3 :    //Eliminazione
                 system(CLEAR);
                 break;
-            case 3 :
-                system(CLEAR);
+            case 4 :    //Ricerche
+                do{
+                    system(CLEAR);
+                    cout << "======================Ricerche======================" << endl;
+                    cout << endl << endl;
+                    cout << " [1] - Ricerca un elemento" << endl;
+                    cout << " [2] - Ricerca il Minimo" << endl;
+                    cout << " [3] - Ricerca il Massimo" << endl;
+                    cout << endl << endl;
+                    cout << " [0] - Esci" << endl << endl;
+                    cout << "Seleziona : ";
+                    cin >> Tasto;
+                    switch( Tasto ){
+                        case 1 :
+                            system(CLEAR);
+                            break;
+                        case 2 :
+                            system(CLEAR);
+                            break;
+                        case 3 :
+                            system(CLEAR);
+                            break;
+                    }
+                }
+                while ( Tasto != 0 );
+                cin.clear();
+                Tasto = -1;
                 break;
-            case 4 :
-                system(CLEAR);
+            case 5 :    //Rotazioni
+                do{
+                    system(CLEAR);
+                    cout << "======================Rotazioni======================" << endl;
+                    cout << endl << endl;
+                    cout << " [1] - Rotazione a DX" << endl;
+                    cout << " [2] - Rotazione a SX" << endl;
+                    cout << endl << endl;
+                    cout << " [0] - Esci" << endl << endl;
+                    cout << "Seleziona : ";
+                    cin >> Tasto;
+                    switch( Tasto ){
+                        case 1 :
+                            system(CLEAR);
+                            break;
+                        case 2 :
+                            system(CLEAR);
+                            break;
+                    }
+                }
+                while ( Tasto != 0 );
+                cin.clear();
+                Tasto = -1;
                 break;
         }
     }
     while ( Tasto != 0 );
+    cin.clear();
     return 0;
 }
 
